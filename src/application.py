41da1898ff8601d951cc2dbf60c7acbcaa82b236
Diego <diego.tavares.leal@gmail.com>
2022-10-16 00:54:00 -0300

    feat: implementing dependecy injector

--- src/application.py
+++ src/application.py
@@ -1,5 +1,6 @@
 from fastapi import FastAPI
 from frameworks.fast_api.manager import FastApiManager
+from frameworks.container import FrameworkContainer
 
 
 def initialize() -> FastAPI:
@@ -8,6 +9,12 @@ def initialize() -> FastAPI:
         app.initialize_cors()
         app.initialize_limiter()
         app.initialize_routers()
+        __initialize_framework_container()
         return app.get_instance()
+
     except Exception as error:
         raise("An excepetion has ocurred trying to initialize FastApi: ", error)
+
+def __initialize_framework_container() -> None:
+        container = FrameworkContainer()
+        container.wire(modules=[__name__])
